1404456d32ea48c02300e1fc30010f379145f8c0
jrosco <j12d@jros.co>
2023-07-21 10:11:08 +1000

    :loud_sound: added simpleGit debug mode (not enabled)

--- src/git/cmd.ts
+++ src/git/cmd.ts
@@ -2,6 +2,10 @@ import simpleGit, { SimpleGit } from 'simple-git';
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 
+// debug simple-git
+// const debug = require('debug');
+// debug.enable('simple-git:task:push:*');
+
 import { GitNotesStatusBar } from '../ui/status';
 import { NotesOutputChannel } from '../ui/output';
 
